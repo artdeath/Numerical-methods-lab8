1eac8e105fd3c2ffcd4cefce5dd887ec3671c198
Vladislav <artd3ath@gmail.com>
2024-12-07 18:51:27 +0700

    Update lab8.cpp

--- lab8.cpp
+++ lab8.cpp
@@ -13,11 +13,9 @@ int main() {
     double y0 = 993; 
 
     double h = 0.0001; 
-    double t_end = 365; // „Ó‰
+    double t_end = 365; // √£√Æ√§
 
     Lotka_Volterra obj(alpha, beta, gamma, delta, x0, y0);
-
-    obj.eulerMethod(h, t_end);
 
     obj.rungeKutta4(h, t_end);
 
